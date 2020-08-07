f8a65da2f13f96f1546172ff4f92e470395db17b
Your Name <you@example.com>
2020-08-07 14:27:24 +0200

    add job views and url

--- job/models.py
+++ job/models.py
@@ -22,7 +22,14 @@ class Job(models.Model): #table
   vacancy = models.IntegerField(default=1)
   salary = models.IntegerField(default=0)
   experience = models.IntegerField(default=1)
+  category = models.ForeignKey('Category', on_delete=models.CASCADE)
 
-  #Return the title of the job 
+  #Return the title of the job
   def __str__(self):
       return self.title
+
+class Category(models.Model):
+  name = models.CharField(max_length=25)
+
+  def __str__(self):
+    return self.name
--- job/views.py
+++ job/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def job_list(request):
+  pass
+
+def job_detail(request, id):
+  pass
